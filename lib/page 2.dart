21c3f0f8c34f95811b472a67ebe7b20b71f9fdd6
ameliashabiyyah <ameliamarhas83@gmail.com>
2024-09-10 12:32:15 +0700

    tambahan tugas latihan 4

--- lib/page 2.dart
+++ lib/page 2.dart
@@ -1,3 +1,40 @@
+// import 'package:flutter/material.dart';
+
+// class page2 extends StatelessWidget {
+//   const page2({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//           child: Row(
+//         children: [
+//           Container(
+//             height: 100,
+//             width: 100,
+//             color: Colors.red,
+//           ),
+//           Container(
+//             height: 100,
+//             width: 100,
+//             color: Colors.green,
+//           ),
+//           Container(
+//             height: 100,
+//             width: 100,
+//             color: Colors.blue,
+//           ),
+//           Container(
+//             height: 100,
+//             width: 100,
+//             color: Colors.yellow,
+//           ),
+//         ],
+//       )),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class page2 extends StatelessWidget {
@@ -6,31 +43,38 @@ class page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Row(
+      body: Row(
         children: [
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
+          Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
+            ],
           ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.yellow,
-          ),
+          Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
+              ),
+            ],
+          )
         ],
-      )),
+      ),
     );
   }
 }
